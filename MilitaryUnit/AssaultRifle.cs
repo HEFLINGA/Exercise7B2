bd94737ae37fed92a0b692996140e4ae3896fe24
Auston Hefling <auston.hefling@outlook.com>
2018-09-21 15:48:14 -0400

    Add child classes and methods under weapon

---
+++ MilitaryUnit/AssaultRifle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryUnit
+{
+    class AssaultRifle : Weapon
+    {
+        public void ARType(string assaultRifleType)
+        {
+            Console.WriteLine($"Assault rifle type: {assaultRifleType}");
+        }
+
+        public void ExtendedMag()
+        {
+            Console.WriteLine("You have an extended mag on this assault rifle!");
+        }
+
+        public void MuzzleBreak()
+        {
+            Console.WriteLine("This weapon has a muzzle break!");
+        }
+    }
+}
---
+++ MilitaryUnit/RocketLauncher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryUnit
+{
+    class RocketLauncher : Weapon
+    {
+        public void RocketLauncherType(string RLType)
+        {
+            Console.WriteLine($"Rocket Launcher Type: {RLType}");
+        }
+
+        public void ExplosionSize()
+        {
+            Console.WriteLine("Explosion Radius: 100m");
+        }
+    }
+}
